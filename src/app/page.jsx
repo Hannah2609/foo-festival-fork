33d012a52748af1af8b19686e05063d3b3b087d5
Katjahkrogh <you@example.com>
2023-12-01 15:17:21 +0100

    footer i page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import Lineup from "./Components/Lineup";
+import Footer from "./components/Footer";
 
 export default function Home() {
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Lineup/>
+      <Footer/>
     </>
   );
 }
