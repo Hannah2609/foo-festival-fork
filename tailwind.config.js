b143638e483383ac18277257496cb474dbc25ffd
Katjahkrogh <you@example.com>
2023-12-01 13:06:00 +0100

    definering af farver i tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        fooBlack: {
+          900: "#000000",
+        },
+        fooBlue: {
+          700: "#0D5272",
+        },
+        fooPink: {
+          900: "#EC5564",
+        },
+        fooYellow: {
+          200: "#FAE499",
+        },
+        fooWhite: {
+          900: "#FFFFFF",
+        },
+        fooGrey: {
+          900: "#2B2B2B",
+        },
       },
     },
   },
   plugins: [],
-}
+};
