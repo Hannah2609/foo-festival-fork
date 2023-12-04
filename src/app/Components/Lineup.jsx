8d91b461c3d6061eb70f443e6cdf493357af7f75
Hannah2609 <hann7419@stud.kea.dk>
2023-12-04 12:27:20 +0100

    .

--- src/app/Components/Lineup.jsx
+++ src/app/Components/Lineup.jsx
@@ -2,23 +2,35 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-function Lineup({ data }) {
-  const [articles, setArticles] = useState([]);
+function Lineup() {
+  const [names, setNames] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/bands")
+
+    fetch('http://localhost:8080/bands?_limit=5')
       .then((res) => res.json())
       .then((data) => {
-        setArticles(data);
+        setNames(data);
       });
   }, []);
 
   return (
     <>
       <h1>Lineup</h1>
-      <ol className="flex flex-wrap gap-4 m-10 justify-center">
-        {articles.map((bands) => (
-          <li key={bands.slug}>{bands.name}</li>
+      <ol className="flex flex-wrap gap-4 m-10 justify-center items-baseline">
+        {names.map((band, index) => (
+          <li
+            key={band.slug}
+            className={
+              index < 4
+                ? "text-6xl "
+                : index < 15
+                ? "text-3xl"
+                : "text-xl"
+            }
+          >
+            {band.name}
+          </li>
         ))}
       </ol>
     </>
--- src/app/band/page.jsx
+++ src/app/band/page.jsx
@@ -1,11 +1,47 @@
-import React, { useState, useEffect } from "react";
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
 function Band() {
+  const [band, setBand] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/bands/a-perfect-circle")
+      .then((res) => res.json())
+      .then((data) => {
+        setBand(data);
+      });
+  }, []);
+
   return (
-    <div>
-        <h1></h1>
-    </div>
-  )
+    <>
+      {/* <Image src={band.logo} className="aspect-square object-contain mx-auto" width={300} height={300} alt="product image" /> */}
+      <div>
+        <h3 className="text-center text-fooYellow-200">{band.genre}</h3>
+        <h1 className="text-center text-4xl">{band.name}</h1>
+      </div>
+
+      <section className="grid sm:grid-cols-2 grid-cols-1 gap-4">
+        <div>
+          <div></div>
+          <div>
+            <h2>Medlemmer</h2>
+            <p>{band.members}</p>
+            {/* <ol>
+              {band.members.map((name, index) => (
+                <li key={`${band.slug}-${index}`}>{name}</li>
+              ))}
+            </ol> */}
+          </div>
+        </div>
+        <article>
+          <p>{band.bio}</p>
+          <p>{band.logoCredits}</p>
+        </article>
+      </section>
+    </>
+  );
 }
 
-export default Band
+export default Band;
