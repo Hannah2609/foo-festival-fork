57163757903eab9c5f661cc37e32fa17dc2bf415
Katjahkrogh <you@example.com>
2023-12-04 14:10:52 +0100

    footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,25 @@
 import Lineup from "./Components/Lineup";
 import Footer from "./components/Footer";
 
+import { Bebas_Neue, Poppins } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "200",
+  display: "swap",
+});
+
 export default function Home() {
-
-
   return (
     <>
-      <Lineup/>
-      <Footer/>
+      <Lineup />
+      <Footer />
     </>
   );
 }
