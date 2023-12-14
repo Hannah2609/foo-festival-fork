f2ec7d6f79db6bfc3ce9efdbd4cc83f3ed36f06f
Katjahkrogh <you@example.com>
2023-12-14 17:31:51 +0100

    singleview done

--- src/app/band/[slug]/page.jsx
+++ src/app/band/[slug]/page.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Image from "next/image";
 import Header from "@/components/Header";
-import styles from "../../../components/Singleview.module.css";
-import { Bebas_Neue } from "next/font/google";
+import SingleView from "@/components/SingleView";
 
-const bebasNeue = Bebas_Neue({
-  subsets: ["latin"],
-  weight: "400",
-  display: "swap",
-});
 
 export const metadata = {
   title: "FooFest | SingleView",
@@ -38,58 +31,7 @@ export default async function Singlepage({ params }) {
   return (
     <>
       <Header />
-      <section className={styles.container}>
-        {logo && logo.startsWith("http") ? (
-          <Image src={logo} fill className="object-cover" alt="band image" />
-        ) : (
-          <Image
-            fill
-            src={`https://robust-ionized-tartan.glitch.me/logos/${logo}`}
-            className="object-cover"
-            alt="band image"
-          />
-        )}
-        <div className="absolute inset-x-0 bottom-0 pt-40 pl-2 bg-gradient-to-t from-black to-transparent-200"></div>
-      </section>
-      <main className="-mt-24 sm:-mt-36 relative z-10">
-        <div>
-          <h3
-            className={`${bebasNeue.className} text-2xl text-center text-fooYellow-200`}
-          >
-            {genre}
-          </h3>
-          <h1
-            className={`${bebasNeue.className} text-center text-5xl sm:text-7xl opacity-100`}
-          >
-            {name}
-          </h1>
-        </div>
-        <section className="grid sm:grid-cols-2 grid-cols-1 gap-10 md:gap-4 mt-10 sm:mt-20 sm:mx-28">
-          <div className="flex items-start gap-3 ">
-            <Image
-              src="/medlemmer.svg"
-              width={40}
-              height={40}
-              alt="musik ikon"
-            />
-            <div className="flex flex-col">
-              <h2 className="text-2xl">Medlemmer</h2>
-              <ul className="mt-4">
-                {members && members.map((name) => <li key={slug}>{name}</li>)}
-              </ul>
-            </div>
-          </div>
-          <article >
-            <h2 className="text-2xl mb-4" >Om {name}</h2>
-            <p>{bio}</p>
-            {logoCredits && (
-              <p className="text-xs text-fooGrey-200 mt-2">
-                Fotocredits: {logoCredits}
-              </p>
-            )}
-          </article>
-        </section>
-      </main>
+      <SingleView data={data} />
     </>
   );
 }
---
+++ src/components/SingleView.jsx
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "./Singleview.module.css";
+import Image from "next/image";
+import { Bebas_Neue } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
+
+function SingleView({ data }) {
+  return (
+    <div>
+      <section className={styles.container}>
+        {data.logo && data.logo.startsWith("http") ? (
+          <Image
+            src={data.logo}
+            fill
+            className="object-cover"
+            alt="band image"
+          />
+        ) : (
+          <Image
+            fill
+            src={`https://robust-ionized-tartan.glitch.me/logos/${data.logo}`}
+            className="object-cover"
+            alt="band image"
+          />
+        )}
+        <div className="absolute inset-x-0 bottom-0 pt-40 pl-2 bg-gradient-to-t from-black to-transparent-200"></div>
+      </section>
+      <motion.section
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        animate={{ y: -35 }}
+        viewport={{ once: true }}
+      >
+        <main className="-mt-32 sm:-mt-40 relative z-10">
+          <div>
+            <h3
+              className={`${bebasNeue.className} text-2xl text-center text-fooYellow-200`}
+            >
+              {data.genre}
+            </h3>
+            <h1
+              className={`${bebasNeue.className} text-center text-5xl sm:text-7xl opacity-100`}
+            >
+              {data.name}
+            </h1>
+          </div>
+          <section className="grid sm:grid-cols-2 grid-cols-1 gap-10 md:gap-4 mt-10 sm:mt-20 sm:mx-28">
+            <div className="flex items-start gap-3 mt-6">
+              <Image
+                src="/medlemmer.svg"
+                width={40}
+                height={40}
+                alt="musik ikon"
+              />
+              <div className="flex flex-col">
+                <h2 className={`${bebasNeue.className} text-2xl`}>Medlemmer</h2>
+                <ul className="mt-4">
+                  {data.members &&
+                    data.members.map((name) => <li key={data.slug}>{name}</li>)}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <h2 className={`${bebasNeue.className} text-2xl mb-4`}>
+                Om {data.name}
+              </h2>
+
+              <p>{data.bio}</p>
+              {data.logoCredits && (
+                <p className="text-xs text-fooGrey-200 mt-2">
+                  Fotocredits: {data.logoCredits}
+                </p>
+              )}
+            </div>
+          </section>
+        </main>
+      </motion.section>
+    </div>
+  );
+}
+
+export default SingleView;
