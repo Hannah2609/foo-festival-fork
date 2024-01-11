706a36e14536ed65fa7bf3056087d58bdebd0d1e
Hannah2609 <hann7419@stud.kea.dk>
2024-01-11 14:16:46 +0100

    button link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,9 +38,8 @@ export default function Home() {
           >
             <Link href="/billetter">Køb billetter</Link>
           </button>
-          <button className="mt-20 animate-bounce" aria-label="go down to lineup">
-
-            <Link href="#lineup" >
+          <button className="mt-20 animate-bounce" >
+            <Link href="#lineup" aria-label="go down to lineup">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="60"
